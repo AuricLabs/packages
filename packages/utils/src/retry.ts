afeef85f979d391a2809e99471e862113e3dcfda
Kevin Upton <k_upton@live.com>
2025-10-01 06:00:52 +1000

    [Add] @auriclabs/utils

---
+++ packages/utils/src/retry.ts
@@ -0,0 +1,24 @@
+import { logger } from '@auriclabs/logger';
+import { merge } from 'lodash-es';
+import pRetry, { FailedAttemptError, Options } from 'p-retry';
+
+export const retry = <T>(fn: () => Promise<T>, options?: Options): Promise<T> => {
+  return pRetry(
+    fn,
+    merge(
+      {
+        retries: 5,
+        factor: 1.5,
+        minTimeout: 1000,
+        maxTimeout: 10000,
+      } satisfies Options, // default options
+      options, // user options
+      {
+        async onFailedAttempt(error: FailedAttemptError) {
+          logger.error({ err: error, attemptNumber: error.attemptNumber }, 'Error retrying');
+          await options?.onFailedAttempt?.(error);
+        },
+      } satisfies Options, // override options
+    ),
+  );
+};
