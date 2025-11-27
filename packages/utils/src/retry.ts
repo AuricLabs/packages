bdf0a0c1edc015a0530de7eb947b76742ecc26a7
Kevin Upton <k_upton@live.com>
2025-11-27 13:06:31 +1100

    improved retry logic

--- packages/utils/src/retry.ts
+++ packages/utils/src/retry.ts
@@ -2,16 +2,23 @@ import { logger } from '@auriclabs/logger';
 import { merge } from 'lodash-es';
 import pRetry, { FailedAttemptError, Options } from 'p-retry';
 
-export const retry = <T>(fn: () => Promise<T>, options?: Options): Promise<T> => {
+export let defaultRetryConfig: Options = {
+  retries: 5,
+  factor: 1.5,
+  minTimeout: 1000,
+  maxTimeout: 10000,
+};
+
+export const setDefaultRetryConfig = (config: Options) => {
+  defaultRetryConfig = config;
+};
+
+export const retry = <T>(fn: () => Promise<T> | T, options?: Options): Promise<T> => {
   return pRetry(
     fn,
     merge(
-      {
-        retries: 5,
-        factor: 1.5,
-        minTimeout: 1000,
-        maxTimeout: 10000,
-      } satisfies Options, // default options
+      {}, // default options
+      defaultRetryConfig,
       options, // user options
       {
         async onFailedAttempt(error: FailedAttemptError) {
